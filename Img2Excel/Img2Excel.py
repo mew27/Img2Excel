40737eca7ff09afeee63aa69b967f5c2ce7fa1b6
Terzolo <Terzolo@GAMING-PC>
2022-11-06 22:37:24 +0100

    removed naughty files

--- Img2Excel/Img2Excel.py
+++ Img2Excel/Img2Excel.py
@@ -3,18 +3,39 @@ from PIL import Image
 import numpy
 import xlsxwriter
 from tqdm import tqdm
+import sys
 
+if len(sys.argv) < 3:
+    print("Usage: python Img2Excel.py image outputfile [ratio]")
+    exit()
 
-im = Image.open("a1ff76b04fd4d2bfcca0285e4c081021.jpeg")
+image_name = sys.argv[1]
+outputfile = sys.argv[2]
+
+if len(sys.argv) >= 4:
+    try:
+        ratio = int(sys.argv[3])
+        print(f"Converting pic with a ratio of 1:{ratio}")
+
+        if ratio < 1:
+            print("Ratio should be a number bigger than 1")
+            exit()
+    except:
+        print("Ratio should be a number bigger than 1")
+        exit()
+else:
+    ratio = 10
+
+im = Image.open(image_name)
 imArray = numpy.asarray(im)
 
-workbook = xlsxwriter.Workbook('converted.xlsx')
+workbook = xlsxwriter.Workbook(outputfile)
 worksheet = workbook.add_worksheet()
 
 
-for i in tqdm(range(0, imArray.shape[0], 10)):
+for i in tqdm(range(0, imArray.shape[0], ratio)):
     worksheet.set_row_pixels(i, 10)
-    for j in range(0, imArray.shape[1], 10):
+    for j in range(0, imArray.shape[1], ratio):
         r = imArray[i][j][0]
         g = imArray[i][j][1]
         b = imArray[i][j][2]
@@ -24,8 +45,10 @@ for i in tqdm(range(0, imArray.shape[0], 10)):
         rgb_format.set_bg_color(rgb_string)
 
         #rgb_format = findRGBPalette(pltte, r, g, b)
-        worksheet.write(i//10, j//10, '', rgb_format)
+        worksheet.write(i//ratio, j//ratio, '', rgb_format)
 
-worksheet.set_column_pixels(0, imArray.shape[1] // 10 + 1, 20)
+worksheet.set_column_pixels(0, imArray.shape[1] // ratio + 1, 20)
 
+print("Writing to file...")
 workbook.close()
+print("Done!")
